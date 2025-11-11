b8137b8320c8446a96389fe8b010413a95f86add
Jeremy Cheng <jeremy.jj.cheng@gmail.com>
2025-11-10 17:29:45 -0800

    Add Boston bike lanes

--- map.js
+++ map.js
@@ -10,10 +10,28 @@ mapboxgl.accessToken =
 
 // Initialize the map
 const map = new mapboxgl.Map({
-  container: "map", // ID of the div where the map will render
-  style: "mapbox://styles/mapbox/streets-v12", // Map style
-  center: [-71.09415, 42.36027], // [longitude, latitude]
-  zoom: 12, // Initial zoom level
-  minZoom: 5, // Minimum allowed zoom
-  maxZoom: 18, // Maximum allowed zoom
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
+
+map.on("load", async () => {
+  map.addSource("boston_route", {
+    type: "geojson",
+    data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson",
+  });
+
+  map.addLayer({
+    id: "bike-lanes",
+    type: "line",
+    source: "boston_route",
+    paint: {
+      "line-color": "#32D400", // Bright green line
+      "line-width": 5, // Thicker
+      "line-opacity": 0.6, // Slightly transparent
+    },
+  });
 });
